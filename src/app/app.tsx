9550b45ea172ddd9e461509f003631aa6df495e1
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2023-03-09 16:42:21 +0200

    Added additional context data

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,4 +1,6 @@
-import { createContext, useMemo, useState } from 'react';
+import { createContext, useEffect, useMemo, useState } from 'react';
+
+import styles from './app.module.scss';
 
 export interface InputData {
   rows: number;
@@ -24,15 +26,29 @@ export const RowsContext = createContext<{
   setRowsAmount: () => void 0,
 })
 
-export const ColumnsContext = createContext(0)
+export const ColumnsContext = createContext<{
+  columnsAmount: number | null
+  setColumnsAmount: React.Dispatch<React.SetStateAction<number | null>>
+}>({
+  columnsAmount: null,
+  setColumnsAmount: () => void 0,
+})
 
-export const NearestContext = createContext(0)
+export const NearestContext = createContext<{
+  nearestAmount: number | null
+  setNearestAmount: React.Dispatch<React.SetStateAction<number | null>>
+}>({
+  nearestAmount: null,
+  setNearestAmount: () => void 0,
+})
 
 export const HoveredCellContext = createContext(0)
 
 export function App() {
   const [rowsAmount, setRowsAmount] = useState<number | null>(null)
-
+  const [columnsAmount, setColumnsAmount] = useState<number | null>(null)
+  const [nearestAmount, setNearestAmount] = useState<number | null>(null)
+  
   const rowsAmountValue = useMemo(
     () => ({
       rowsAmount,
@@ -41,10 +57,38 @@ export function App() {
     [rowsAmount],
   )
   
+  const columnsAmountValue = useMemo(
+    () => ({
+      columnsAmount,
+      setColumnsAmount,
+    }),
+    [columnsAmount],
+  )
+    
+  const nearestAmountValue = useMemo(
+    () => ({
+      nearestAmount,
+      setNearestAmount,
+    }),
+    [nearestAmount],
+  )
+
+  useEffect(() => {
+    if (rowsAmount !== null && columnsAmount !== null) {
+      const cellAmount = rowsAmount * columnsAmount
+      setNearestAmount((nearestAmount || 0) >= cellAmount ? cellAmount - 1 : nearestAmount)
+    }
+  }, [rowsAmount, columnsAmount])
+  
   return (
     <RowsContext.Provider value={rowsAmountValue}>
-      
-
+      <ColumnsContext.Provider value={columnsAmountValue}>
+        <NearestContext.Provider value={nearestAmountValue}>
+          <div className={styles.container}>
+          
+          </div>
+        </NearestContext.Provider>
+      </ColumnsContext.Provider>
     </RowsContext.Provider>
 
   );
