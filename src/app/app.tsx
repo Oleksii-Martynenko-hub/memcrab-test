0858eacfc53174fcc52155f65eca3742fa4e14af
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2023-03-09 15:43:52 +0200

    Added base data structure

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,15 +1,52 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
+import { createContext, useMemo, useState } from 'react';
 
-import NxWelcome from './nx-welcome';
+export interface InputData {
+  rows: number;
+  columns: number;
+  nearest: number;
+}
+
+export type Cell = {
+  id: number,
+  amount: number
+}
+
+export type Row = {
+  id: number;
+  cells: Cell[];
+}
+
+export const RowsContext = createContext<{
+  rowsAmount: number | null
+  setRowsAmount: React.Dispatch<React.SetStateAction<number | null>>
+}>({
+  rowsAmount: null,
+  setRowsAmount: () => void 0,
+})
+
+export const ColumnsContext = createContext(0)
+
+export const NearestContext = createContext(0)
+
+export const HoveredCellContext = createContext(0)
 
 export function App() {
-  return (
-    <>
-      <NxWelcome title="memcrab-test" />
+  const [rowsAmount, setRowsAmount] = useState<number | null>(null)
 
-      <div />
-    </>
+  const rowsAmountValue = useMemo(
+    () => ({
+      rowsAmount,
+      setRowsAmount,
+    }),
+    [rowsAmount],
+  )
+  
+  return (
+    <RowsContext.Provider value={rowsAmountValue}>
+      
+
+    </RowsContext.Provider>
+
   );
 }
 
