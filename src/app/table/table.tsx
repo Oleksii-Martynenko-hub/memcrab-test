dc6576228a1b1df16a5c3443dbac99587cb443f3
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2023-03-10 10:11:51 +0200

    Added average row

--- src/app/table/table.tsx
+++ src/app/table/table.tsx
@@ -1,11 +1,20 @@
-import { Row } from '../app';
+import { useContext } from 'react';
+import { ColumnsContext, Row } from '../app';
 import styles from './table.module.scss';
 
 export interface TableProps {
   rows: Row[]
+  setRows: React.Dispatch<React.SetStateAction<Row[]>>
 }
 
-export function Table({ rows }: TableProps) {
+export function Table({ rows, setRows }: TableProps) {
+  const { columnsAmount } = useContext(ColumnsContext)
+
+  const average = (arr: number[]) => roundTo(arr.reduce( ( p, c ) => p + c, 0 ) / arr.length, 2);
+
+  const roundTo = (n: number, to: number) => {    
+    return +(Math.round(+(n + "e+" + to)) + "e-" + to);
+}
 
   return (
     <div className={styles['container']}>
@@ -21,6 +30,13 @@ export function Table({ rows }: TableProps) {
             </tr>
             
             ))}
+
+          <tr key='average'>
+            {[...Array(columnsAmount).keys()].map((i) => (
+              <td key={i}>{average(rows.map(({ cells }) => cells[i] ? cells[i].amount : 0))}</td>
+            ))}
+            <td key='sum-empty-cell'></td>
+          </tr>
         </tbody>
       </table>
     </div>
